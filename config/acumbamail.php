06a4fa64b4160b58d5b0b3736a29dfe8682568d8
Laurens Kuiper <l@urens.me>
2026-03-27 12:01:45 +0100

    chore: scaffold project with composer, vite, phpunit, and config

---
+++ config/acumbamail.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Acumbamail Auth Token
+    |--------------------------------------------------------------------------
+    |
+    | The auth token is required to authenticate your application with
+    | Acumbamail. You can find this token in the API section of your
+    | Acumbamail account settings.
+    |
+    */
+
+    'auth_token' => env('ACUMBAMAIL_AUTH_TOKEN'),
+
+];
